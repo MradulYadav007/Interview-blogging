26f62c6207acc19babde2662cf2619b4a146489f
MradulYadav007 <mradul.mr20@gmail.com>
2021-03-17 09:04:18 +0530

    server setup

---
+++ server/app.js
@@ -0,0 +1,28 @@
+const express=require('express');
+const app=express();
+const cors=require('cors');
+const dotenv=require('dotenv');
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+//create
+app.post('/insert',(request,response)=>{
+
+});
+
+//read
+app.get('/getAll',(request,response)=>{
+    console.log('test');
+});
+
+//update
+
+
+//delete
+
+app.listen(5000,() => {
+    console.log('App is running');
+});
